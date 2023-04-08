5e7954f51210f78ea11d83b255fd67a79ebcb01c
ohgus <anakin1108@naver.com>
2023-04-08 18:37:51 +0900

    230408_5수정

--- b1181/boj1181.js
+++ b1181/boj1181.js
@@ -7,6 +7,7 @@ for (i = 1; i <= n; i++){
     arr1.push (input[i]);
 }
 let arr = [...new Set(arr1)];
+// 중복을 제거하고 배열에 추가
 arr.sort((a,b) => {
     if (a.length !== b.length) return a.length - b.length;
     else {
@@ -15,6 +16,7 @@ arr.sort((a,b) => {
         else return 0;
     } 
 })
+// 배열을 처음에는 길이순으로 만약 같다면 오름차순으로 정렬
 
 let answer = '';
 for (j = 0; j < arr.length; j++ ){
