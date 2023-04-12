a4b32e50a0f8461c654ed5718036ff9b0f39f8b5
ohgus <anakin1108@naver.com>
2023-04-12 11:44:16 +0900

    230412_1설명추가

--- b2805/boj2805.js
+++ b2805/boj2805.js
@@ -12,13 +12,13 @@ let cut = 0;
 while (start <= end){
     let mid = parseInt((start + end)/ 2);
     let cutSum = 0;
-    for (x of treeLen){
+    for (x of treeLen){ // 현 나무의 길이가 남겨놓을 나무의 길이보다 길다면.
         if (x > mid){cutSum += x - mid;}
     }
-    if (cutSum < need){
+    if (cutSum < need){ //자를 나무의 양이 필요한 양보다 적다면.
         end = mid - 1;
     }
-    else {
+    else { // 필요한 양보다 많거나 같다면.
         cut = mid;
         start = mid +1;
     }
