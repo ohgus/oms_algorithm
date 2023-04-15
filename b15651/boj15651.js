df1a2f4ee0a1afa2ee7e6657c6a2229d7f24548d
ohgus <anakin1108@naver.com>
2023-04-15 17:42:30 +0900

    230415_3

---
+++ b15651/boj15651.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync('./b15651/input15651.txt').toString().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+let arr = [];
+for (let i = 1; i <= n; i++) arr.push(i);
+let visited = new Array(n).fill(false);
+let selected = [];
+
+let answer = '';
+function dfs (arr, depth){
+    if (depth === m){
+        let result = [];
+        for (let i of selected) result.push(arr[i]);
+        for (let x of result) answer += x + ' ';
+        answer += '\n';
+        return;
+    }
+    for (let i = 0; i < arr.length; i ++){
+        selected.push(i);
+        visited[i] = true;
+        dfs(arr, depth + 1);
+        selected.pop();
+        visited[i] = false;
+    }
+}
+dfs(arr, 0);
+console.log(answer);
