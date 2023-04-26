237796c15c0879c802995e6aeef505d1dfa4589b
ohgus <anakin1108@naver.com>
2023-04-26 17:26:27 +0900

    230436_2수정

--- p23/p23.js
+++ p23/p23.js
@@ -3,16 +3,13 @@ const cards2 = ["want", "to"];
 const goal = ["i", "want", "to", "drink", "water"];
 function solution(cards1, cards2, goal) {
     var answer = 'Yes';
-    let result = '';
     let c1 = cards1; // 본 배열의 값을 바꾸지 않고 사용할 임시 배열
     let c2 = cards2;
     goal.forEach((e) => { // goal 의 인덱스를 하나씩 확인하며 각 카드뭉치에 첫번째가 같은게 있는지 확인한다.
         if(e === c1[0]) { // 1번 카드뭉치에 있는지 확인
-            result += e;
             c1.shift();
         }
         else if(e === c2[0]){ // 2번 카드뭉치에 있는지 확인
-            result += e;
             c2.shift();
         }
         else { // 1번,2번 모두 없을 경우
