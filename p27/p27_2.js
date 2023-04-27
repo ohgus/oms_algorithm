ea9d1a5e57b9e65a1ea12ce39c91afb748aaf16e
ohgus <anakin1108@naver.com>
2023-04-28 00:08:08 +0900

    230428_1수정

--- p27/p27_2.js
+++ p27/p27_2.js
@@ -5,26 +5,26 @@ function solution(n, wires) {
     let answer = Number.MAX_SAFE_INTEGER;
     const arr = Array.from(Array(n + 1),() => []);
     
-    wires.map((e) => {
+    wires.map((e) => { // 각 노드들의 인접한 노드를 저장한다.
         let [a,b] = e;
         arr[a].push(b);
         arr[b].push(a);
     })
     
-    function st (r,d){
+    function st (r,d){ // 현재 노드와 삭제할 노드
         let cnt = 0;
         let queue = [r];
         let visit = [];
         visit[r] = true;
         while(queue.length){
             let idx = queue.pop();
-            arr[idx].forEach((e) => {
+            arr[idx].forEach((e) => { // 현재 노드의 인접한 노드들을 방문
                 if(e !== d && visit[e] !== true){
-                    visit[e] = true;
-                    queue.push(e);
+                    visit[e] = true; // 방문처리
+                    queue.push(e); // 방문한 노드의 인접한 노드들을 방문하기 위해 큐에 담아준다.
                 }
             })
-            cnt++;
+            cnt++; // 큐에서 처리가 끝난 노드가 나올 때 마다 수를 세어준다.
         }
         return cnt;
     }
