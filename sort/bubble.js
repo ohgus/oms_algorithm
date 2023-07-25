2387da8d2e4be94c30df1cd2b29e3dbd43784e51
ohgus <anakin1108@naver.com>
2023-07-25 23:32:04 +0900

    bubble sort

---
+++ sort/bubble.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+let arr = Array.from({ length: 30000}, () => Math.floor(Math.random() * 1000));
+startTime = new Date().getTime();
+bubbleSort(arr);
+endTime = new Date().getTime();
+console.log(`버블 정렬 소요 시간: ${endTime - startTime}ms`);
